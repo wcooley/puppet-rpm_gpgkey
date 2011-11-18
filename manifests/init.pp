6edb45054d1bfb1f96cc69ed8f0665e7797b3842
Wil Cooley <wcooley@nakedape.cc>
2011-11-18 13:16:34 -0800

    Added new rpm_gpgkey defined type

---
+++ manifests/init.pp
@@ -0,0 +1,7 @@
+
+define rpm_gpgkey($path, $keyid) {
+    exec { "rpm-gpg-import-$name":
+        command => "/bin/rpm --import $path",
+        unless  => "/bin/rpm --quiet -q gpg-pubkey-$keyid"
+    }
+}
