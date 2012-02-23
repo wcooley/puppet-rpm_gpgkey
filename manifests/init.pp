1d9743b8de27e9c1da08de6318a47bf4a4e5fcb7
Wil Cooley <wcooley@nakedape.cc>
2012-02-23 13:22:49 -0800

    Test that key file exists before attempting to import it

--- manifests/init.pp
+++ manifests/init.pp
@@ -2,6 +2,7 @@
 define rpm_gpgkey($path, $keyid) {
     exec { "rpm-gpg-import-$name":
         command => "/bin/rpm --import $path",
-        unless  => "/bin/rpm --quiet -q gpg-pubkey-$keyid"
+        unless  => "/bin/rpm --quiet -q gpg-pubkey-$keyid",
+        onlyif  => "test -f $path",
     }
 }
