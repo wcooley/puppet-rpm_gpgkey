fb8f13b6dd6c3d97422fc65daddadcee6872291b
Wil Cooley <wcooley@nakedape.cc>
2012-02-23 14:20:25 -0800

    Fixes lack of path to 'test'

--- manifests/init.pp
+++ manifests/init.pp
@@ -3,6 +3,6 @@ define rpm_gpgkey($path, $keyid) {
     exec { "rpm-gpg-import-$name":
         command => "/bin/rpm --import $path",
         unless  => "/bin/rpm --quiet -q gpg-pubkey-$keyid",
-        onlyif  => "test -f $path",
+        onlyif  => "/usr/bin/test -f $path",
     }
 }
