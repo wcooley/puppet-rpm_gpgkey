af9d50d1f34b6ca58e568d667d7353c635c6b430
Wil Cooley <wcooley@nakedape.cc>
2013-01-17 11:59:23 -0800

    Add check for URLs as rpm_gpgkey path

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,5 +1,9 @@
 
 define rpm_gpgkey($path, $keyid) {
+  if $path =~ /^(f|ht)tps?:/ {
+    fail('rpm_gpgkey does not work with remote paths!')
+  }
+
   exec { "rpm-gpg-import-$name":
     command => "/bin/rpm --import $path",
     unless  => "/bin/rpm --quiet -q gpg-pubkey-$keyid",
