218f3658585266d9e5951e0e57c009ef093096fc
Wil Cooley <wcooley@nakedape.cc>
2013-01-17 11:50:19 -0800

    Re-indent

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,8 +1,8 @@
 
 define rpm_gpgkey($path, $keyid) {
-    exec { "rpm-gpg-import-$name":
-        command => "/bin/rpm --import $path",
-        unless  => "/bin/rpm --quiet -q gpg-pubkey-$keyid",
-        onlyif  => "/usr/bin/test -f $path",
-    }
+  exec { "rpm-gpg-import-$name":
+    command => "/bin/rpm --import $path",
+    unless  => "/bin/rpm --quiet -q gpg-pubkey-$keyid",
+    onlyif  => "/usr/bin/test -f $path",
+  }
 }
