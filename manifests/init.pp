bac7e8114973213546bc62d482b2503f8fa6a3cc
Eric Eisenhart <eric.eisenhart@sonoma.edu>
2019-12-09 11:21:29 -0800

    Move local path changes inside part of conditional dealing with local paths

--- manifests/init.pp
+++ manifests/init.pp
@@ -20,25 +20,24 @@
 #   Key ID as string.
 #
 define rpm_gpgkey($path, $keyid) {
-
-  if $path !~ /^\// {
-    $realpath = "/etc/pki/rpm-gpg/${path}"
-  }
-  else {
-    $realpath = $path
-  }
-
   # quote keyid to explicitly stringify it, as hiera will return an
   # integer if the hexadecimal number includes only digits 0-9. downcase()
   # doesn't know what to do with an integer.
   $realkeyid = downcase("${keyid}")
 
   if $path =~ /^(f|ht)tps?:/ {
+    # If remote URL for key, just try to do it
     exec { "rpm-gpg-import-${name}":
       command => "/bin/rpm --import ${realpath}",
       unless  => "/bin/rpm --quiet -q gpg-pubkey-${realkeyid}",
     }
   } else {
+    if $path !~ /^\// {
+      $realpath = "/etc/pki/rpm-gpg/${path}"
+    }
+    else {
+      $realpath = $path
+    }
     exec { "rpm-gpg-import-${name}":
       command => "/bin/rpm --import ${realpath}",
       unless  => "/bin/rpm --quiet -q gpg-pubkey-${realkeyid}",
